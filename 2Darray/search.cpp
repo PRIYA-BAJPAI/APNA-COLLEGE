6bf16d57e3c1a3a7a9969a2f9d7b20e3b4d877f1
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-10-24 12:23:43 +0530

    edit

---
+++ 2Darray/search.cpp
@@ -0,0 +1,14 @@
+//leetcode 74
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[3][3];
+    cout<<"input elements"<<endl;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cin>>arr[i][j];
+        }
+    }
+    
+    return 0;
+}

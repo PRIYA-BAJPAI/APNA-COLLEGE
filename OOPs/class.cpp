e17d987f60dfb678bfcf0228a0527fd9f8cd5d15
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-11-22 14:16:42 +0530

    calling class

---
+++ OOPs/class.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class Teacher{
+    public:
+    //properties/attributes
+    string name;
+    string department;
+    double salary;
+    //methods/member function
+    void change(string newdept){
+        department=newdept;
+    }
+};
+int main(){
+    Teacher t1;
+    t1.name="Priya";
+    cout<<t1.name;
+    return 0;
+}

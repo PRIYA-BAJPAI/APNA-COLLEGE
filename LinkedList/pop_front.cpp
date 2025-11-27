ec2b7f19ca39b66a26650b00ab7d49e6ca7d6047
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-11-27 21:21:07 +0530

    LL

---
+++ LinkedList/pop_front.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node *next;
+    node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+class list{
+    node *head;
+    node *tail;
+    public:
+    list(){
+        head=tail=NULL;
+    }
+    void push_front(int val){
+        node *newnode=new node(val);
+        if (head==NULL){
+            head=tail=newnode;
+        }else{
+            newnode->next=head;
+            head=newnode;
+        }
+    }
+    void pop_front(){
+        node *temp=head;
+        if(head==NULL){
+            cout<<"LL is empty"<<endl;
+            return;
+        }else{
+            head=head->next;
+            temp->next=NULL;
+            delete temp;
+        }
+    }
+    void printLL(){
+        node *temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<" -> ";
+            temp=temp->next;
+        }
+        cout<<"NULL"<<endl;
+    }
+};
+int main(){
+    list l;
+    l.push_front(1);
+    l.push_front(2);
+    l.push_front(3);
+    l.push_front(4);
+    l.printLL();
+    l.pop_front();
+    cout<<"After popping"<<endl;
+    l.printLL();
+    return 0;
+}

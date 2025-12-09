2f86e52c820bd6a387cdec8036f22d0b24bd78cc
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-12-09 20:00:03 +0530

    delete

---
+++ LinkedList/Circular/deleteAtTail.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int val){
+        data=val;
+        next=NULL;
+        prev=NULL;
+    }
+};
+class Circular{
+    Node *head;
+    Node *tail;
+    public:
+    Circular(){
+        head=tail=NULL;
+    }
+    void insertathead(int val){
+        Node *newNode=new Node(val);
+        if(head==NULL){
+            head=tail=newNode;
+            tail->next=head;
+        }else{
+            newNode->next=head;
+            head=newNode;
+            tail->next=head;
+        }
+    }
+    void deleteAttail(){
+        if(head==NULL){
+            return ;
+        }else if(tail==head){
+            delete tail;
+            tail=head=NULL;
+        }
+        Node *temp=tail;
+        Node *prev=head;
+        while(prev->next!=tail){
+            prev=prev->next;
+        }
+        tail=prev;
+        tail->next=head;
+        temp->next=NULL;
+        delete temp;
+    }
+    void print(){
+        cout<<head->data<<" -> ";
+        Node *temp=head->next;
+        while(temp!=head){
+            cout<<temp->data<<" -> ";
+            temp=temp->next;
+        }
+        cout<<head->data;
+    }
+};
+int main(){
+    Circular c;
+    c.insertathead(1);
+    c.insertathead(2);
+    c.insertathead(3);
+    c.insertathead(4);
+    c.print();
+    cout<<endl;
+    cout<<"After deletion"<<endl;
+    c.deleteAttail();
+    c.print();
+    return 0;
+}

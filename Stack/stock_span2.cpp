fe2f66f938af6e0c92b518dad7d18a3370b3ae0f
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-12-25 18:06:13 +0530

    another method

---
+++ Stack/stock_span2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int>price={100,80,60,70,60,75,85};
+    vector<int> ans(price.size()); //imp to declare the size of ans
+    stack<int>st;
+    for(int i=0;i<price.size();i++){
+        while(st.size()>0 && price[st.top()] <= price[i]){
+            st.pop();
+        }
+        if(st.empty()){
+            ans[i]=i+1;
+        }else{
+            ans[i]=i-st.top();
+        }
+        st.push(i);
+    }
+    for(int val:ans){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
+

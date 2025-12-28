7bf397c9a80f675544c883abfc211e70b03794f9
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-12-28 18:02:38 +0530

    stack

---
+++ Stack/previous_smaller.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>vec={6,8,0,1,3};
+    stack<int>st;
+    vector<int>ans(vec.size());
+    int n=vec.size();
+    for(int i=0;i<n;i++){
+        while(!st.empty() && st.top()>=vec[i]){
+            st.pop();
+        }
+        if(st.empty()){
+            ans[i]=-1;
+        }else{
+            ans[i]=st.top();
+        }
+        st.push(vec[i]);
+    }
+    for(int val:ans){
+        cout<<val<<" ";
+    }
+    return 0;
+}
